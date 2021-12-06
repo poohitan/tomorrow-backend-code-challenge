035680c92cb7a748ff55f82a5095a71388fd072f
poohitan <poohitan@gmail.com>
2021-12-07 00:32:12 +0200

    Add index route handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,8 @@ const router = express.Router();
 router.use('/activities', activities);
 router.use('/risks', risks);
 
+router.get('/', (req, res) => {
+  res.send('Check out the API documentation for the list of routes at https://github.com/poohitan/tomorrow-backend-code-challenge');
+});
+
 module.exports = router;
